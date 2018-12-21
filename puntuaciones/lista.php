80ad570adce4d7f372827dc996f25bf7d937f9fc
CodingAleCR <aulate@soin.co.cr>
2018-12-21 12:03:30 -0600

    fixing db connection

--- puntuaciones/lista.php
+++ puntuaciones/lista.php
@@ -1,10 +1,5 @@
 <?php
-$con = new pg_connect(
-    "host=ec2-174-129-25-182.compute-1.amazonaws.com port=5432 
-    dbname=dfkl0aljghn8kt 
-    user=rbplczgwhcoxmo 
-    password=08098b73d31e842c91345d0464308801f1d09578d88acb0523ac6767fc0b5fab"
-); 
+$con = pg_connect(getenv("DATABASE_URL"));
 if ($con->connect_errno) {
     echo 'Error al conectar base de datos: ', $con->connect_error;
     exit(); 
