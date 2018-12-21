5db867e5ad2ea495606b211b903b2e51fa8042c6
CodingAleCR <aulate@soin.co.cr>
2018-12-21 13:39:50 -0600

    rows

--- puntuaciones/lista.php
+++ puntuaciones/lista.php
@@ -11,23 +11,24 @@ echo "Preparing statement.<br/>";
 $sql = 'SELECT puntos, nombre FROM puntuaciones ORDER BY fecha DESC';
 
 if (isset($_GET['max'])) {
-   $sql .= ' LIMIT ?';
+   $sql .= " LIMIT ".$_GET['max'];
 }
 
 echo "Statement prepared: $sql.<br/>";
-$result = pg_query($con, "SELECT puntos, nombre FROM puntuaciones ORDER BY fecha DESC");
+$result = pg_query($con, $sql);
 if (!$result) {
     echo "An error occurred.<br/>";
     exit;
 }
 $rows = pg_num_rows($result);
-
 echo $rows . " row(s) returned.\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    foreach ($line as $col_value) {
-        echo "$col_value<br />\n";
-    }
-}
+
+var_dump($result);
+// while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+//     foreach ($line as $col_value) {
+//         echo "$col_value<br />\n";
+//     }
+// }
 pg_free_result($result);
 echo "Closing connection.<br/>";
 // Closing connection
