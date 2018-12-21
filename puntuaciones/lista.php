4ed76a3e962c64574a1e09ee45441ca3c6e8743d
CodingAleCR <aulate@soin.co.cr>
2018-12-21 12:42:54 -0600

    help

--- puntuaciones/lista.php
+++ puntuaciones/lista.php
@@ -1,12 +1,12 @@
 <?php
-echo "Opening connection.\n";
+echo "Opening connection.<br/>";
 $con = pg_connect(getenv("DATABASE_URL"));
 if ($con->connect_errno) {
     echo 'Error al conectar base de datos: ', $con->connect_error;
     exit(); 
 }
 
-echo "Preparing statement.\n";
+echo "Preparing statement.<br/>";
 
 $sql = 'SELECT puntos, nombre FROM puntuaciones ORDER BY fecha DESC';
 
@@ -14,18 +14,19 @@ if (isset($_GET['max'])) {
    $sql .= ' LIMIT ?';
 }
 
-echo "Statement prepared: $sql.\n";
+echo "Statement prepared: $sql.<br/>";
 $result = pg_query($con, $sql);
 if (!$result) {
-    echo "An error occurred.\n";
+    echo "An error occurred.<br/>";
     exit;
 }
-echo "Rows fetched!\n";
-while ($score = pg_fetch_object($result)) {
-    echo $score->puntos." - ".$score->nombre."\n";
-}
-
-echo "Closing connection.\n";
+echo "Rows fetched!<br/>";
+while ($data = pg_fetch_object($result)) {
+    echo $data->puntos . " - ";
+    echo $data->nombre . "<br />";
+  }
+pg_free_result($result);
+echo "Closing connection.<br/>";
 // Closing connection
 pg_close($con);
 ?>
