b4db162a59f27de07a3528cc174b6e0176546dfa
CodingAleCR <aulate@soin.co.cr>
2018-12-21 13:47:44 -0600

    rows

--- puntuaciones/lista.php
+++ puntuaciones/lista.php
@@ -1,12 +1,5 @@
 <?php
-echo "Opening connection.<br/>";
-$con = pg_connect(getenv("DATABASE_URL"));
-if ($con->connect_errno) {
-    echo 'Error al conectar base de datos: ', $con->connect_error;
-    exit(); 
-}
-
-echo "Preparing statement.<br/>";
+$con = pg_connect(getenv("DATABASE_URL")) or die ("Could not connect to server\n"); 
 
 $sql = 'SELECT puntos, nombre FROM puntuaciones ORDER BY fecha DESC';
 
@@ -15,22 +8,13 @@ if (isset($_GET['max'])) {
 }
 
 echo "Statement prepared: $sql.<br/>";
-$result = pg_query($con, $sql);
-if (!$result) {
-    echo "An error occurred.<br/>";
-    exit;
-}
-$rows = pg_num_rows($result);
-echo $rows . " row(s) returned.\n";
+$result = pg_query($con, $sql) or die("Cannot execute query: $query\n");
 
-var_dump($result);
-// while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-//     foreach ($line as $col_value) {
-//         echo "$col_value<br />\n";
-//     }
-// }
+while ($row = pg_fetch_assoc($result)) {
+    echo $row['puntos'] . " - " . $row['nombre'];
+    echo "\n";
+}
+
 pg_free_result($result);
-echo "Closing connection.<br/>";
-// Closing connection
 pg_close($con);
 ?>
